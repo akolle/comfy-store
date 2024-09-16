eeaaed33262af08af41dbeb4c21d5bbe834e94ab
akolle <andykoller55@yahoo.com>
2024-09-15 21:33:07 -0400

    fetch single product

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,4 +1,18 @@
+import { useLoaderData } from 'react-router-dom'
+import { formatPrice, customFetch } from '../utils'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
+export const loader = async ({ params }) => {
+  const response = await customFetch(`/products/${params.id}`)
+  return { product: response.data.data }
+}
+
 const SingleProduct = () => {
-  return <div>SingleProduct</div>
+  const { product } = useLoaderData()
+  const { Image, title, price, description, colors, company } =
+    product.attributes
+  const dollarAmount = formatPrice(price)
+  return <h1 className="text-4xl">SingleProduct</h1>
 }
 export default SingleProduct

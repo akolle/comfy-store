c5219c8e59c81f627853186f4cd54a87dd9b1e01
akolle <andykoller55@yahoo.com>
2024-09-18 17:28:46 -0400

    pagination - completed

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -7,8 +7,14 @@ const PaginationContainer = () => {
   const pages = Array.from({ length: pageCount }, (_, index) => {
     return index + 1
   })
+
+  const { search, pathname } = useLocation()
+  const navigate = useNavigate()
+
   const handlePageChange = (pageNumber) => {
-    console.log(pageNumber)
+    const searchParams = new URLSearchParams(search)
+    searchParams.set('page', pageNumber)
+    navigate(`${pathname}?${searchParams.toString()}`)
   }
 
   if (pageCount < 2) return null
@@ -19,7 +25,9 @@ const PaginationContainer = () => {
         <button
           className="btn btn-xs sm:btn-md join-item"
           onClick={() => {
-            handlePageChange('prev')
+            let prevPage = page - 1
+            if (prevPage < 1) prevPage = pageCount
+            handlePageChange(prevPage)
           }}
         >
           Prev
@@ -40,7 +48,9 @@ const PaginationContainer = () => {
         <button
           className="btn btn-xs sm:btn-md join-item"
           onClick={() => {
-            handlePageChange('next')
+            let nextPage = page + 1
+            if (nextPage > pageCount) nextPage = 1
+            handlePageChange(nextPage)
           }}
         >
           Next

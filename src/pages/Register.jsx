1192e7fe3653b5d5d9f458e1698b22b20ee08be5
akolle <andykoller55@yahoo.com>
2024-09-22 20:46:16 -0400

    remove default values in register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -29,24 +29,9 @@ const Register = () => {
         className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
       >
         <h4 className="text-center text-3xl font-bold">Register</h4>
-        <FormInput
-          type="text"
-          label="username"
-          name="username"
-          defaultValue="james100 smith"
-        />
-        <FormInput
-          type="email"
-          label="email"
-          name="email"
-          defaultValue="james100smith@gmail.com"
-        />
-        <FormInput
-          type="password"
-          label="password"
-          name="password"
-          defaultValue="secret"
-        />
+        <FormInput type="text" label="username" name="username" />
+        <FormInput type="email" label="email" name="email" />
+        <FormInput type="password" label="password" name="password" />
         <div className="mt-4">
           <SubmitBtn text="register" />
         </div>

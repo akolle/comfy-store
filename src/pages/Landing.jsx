08e06ba0b02de76405ea2a40161ca9e3bf2fb2b6
akolle <andykoller55@yahoo.com>
2024-09-24 18:07:52 -0400

    landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,8 +2,14 @@ import { FeaturedProducts, Hero } from '../components'
 import { customFetch } from '../utils'
 const url = '/products?featured=true'
 
+const featuredProductsQuery = {
+  queryKey: ['featuredProducts'],
+  queryFn: () => customFetch(url),
+}
+
 export const loader = (queryClient) => async () => {
-  const response = await customFetch(url)
+  const response = await queryClient.ensureQueryData(featuredProductsQuery)
+  console.log(response)
   const products = response.data.data
   return { products }
 }

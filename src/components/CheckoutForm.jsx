0636052bd61b112f918d802481157097fc9183a2
akolle <andykoller55@yahoo.com>
2024-09-23 22:23:57 -0400

    auth errors

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -37,6 +37,7 @@ export const action =
         error?.response?.data?.error?.message ||
         'there was an error placing your order'
       toast.error(errorMessage)
+      if (error.response.status === 401) return redirect('/login')
       return null
     }
   }
